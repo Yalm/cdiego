be7c760b92de7929c4507e9aeac7b4ad39f2b31b
Yalm <mcondori@dtransforma.com>
2020-02-06 22:12:05 -0500

    2

--- src/orders/repositories/order-detail.repository.ts
+++ src/orders/repositories/order-detail.repository.ts
@@ -6,11 +6,12 @@ export class OrderDetailRepository extends Repository<OrderToProduct> {
 
     topProduct(filter: { date_init: string, date_end: string, skip: number, take: number }): Promise<[any[], number]> {
         let query = this.createQueryBuilder('order_details')
-            .select('products.name')
+            .select(['products.name', 'products.id'])
             .addSelect("SUM(order_details.quantity)", "quantity")
             .innerJoin("order_details.product", "products")
             .innerJoin("order_details.order", "orders")
             .groupBy('products.name')
+            .addGroupBy('products.id')
             .where('orders."stateId" = :state', { state: 2 });
         if (filter.date_init) {
             query = query.andWhere('orders."createdAt" >= :date_init', filter)
