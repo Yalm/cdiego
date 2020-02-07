53f1f10488fcee11c194a1405f8cbbb9a18684a5
Yalm <mcondori@dtransforma.com>
2020-02-06 21:19:23 -0500

    2

--- src/orders/repositories/order-detail.repository.ts
+++ src/orders/repositories/order-detail.repository.ts
@@ -6,14 +6,14 @@ export class OrderDetailRepository extends Repository<OrderToProduct> {
 
     topProduct(filter: { date_init: string, date_end: string, skip: number, take: number }): Promise<[any[], number]> {
         let query = this.createQueryBuilder('order_details')
-            .select(['product.id', 'product.name'])
+            .select(['products.id', 'products.name'])
             .addSelect("SUM(order_details.quantity)", "quantity")
-            .innerJoin("order_details.product", "product")
-            .innerJoin("order_details.order", "order")
-            .where("order.stateId = :state", { state: 2 });
+            .innerJoin("order_details.product", "products")
+            .innerJoin("order_details.order", "orders")
+            .where("orders.stateId = :state", { state: 2 });
         if (filter.date_init) {
-            query = query.andWhere('order.createdAt >= :date_init', filter)
-                .andWhere('order.createdAt < :date_end', filter);
+            query = query.andWhere('orders.createdAt >= :date_init', filter)
+                .andWhere('orders.createdAt < :date_end', filter);
         }
         return Promise.all([
             query.limit(filter.take).offset(filter.skip).orderBy("quantity", 'DESC').getMany(),
