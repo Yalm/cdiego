8ca9c0f02523218e87a2471b7d86bd0dbb4539f5
Yalm <mcondori@dtransforma.com>
2020-02-08 12:59:05 -0500

    ILIKE

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -7,7 +7,7 @@ export class ProductRepository extends Repository<Product> {
     paginate(filter: { search: string, category: string,limit: number, skip: number }): Promise<[Product[], number]> {
         let query = this.createQueryBuilder('product')
             .innerJoinAndSelect("product.category", "category");
-        if (filter.search) query = query.where("product.name like :name", { name: `%${filter.search}%` });
+        if (filter.search) query = query.where("product.name ILIKE :name", { name: `%${filter.search}%` });
         if (filter.category) query = query.andWhere("category.name like :category", filter);
         return Promise.all([
             query.limit(filter.limit || 10).offset(filter.skip || 0).getMany(),
