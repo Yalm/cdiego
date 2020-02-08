3996df686bdeafae7bc819a70974641c809d9014
Yalm <mcondori@dtransforma.com>
2020-02-08 13:09:54 -0500

    orWhere

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -4,10 +4,14 @@ import { Repository, EntityRepository } from "typeorm";
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
 
-    paginate(filter: { search: string, category: string,limit: number, skip: number }): Promise<[Product[], number]> {
+    paginate(filter: { search: string, category: string, limit: number, skip: number }): Promise<[Product[], number]> {
         let query = this.createQueryBuilder('product')
             .innerJoinAndSelect("product.category", "category");
-        if (filter.search) query = query.where("product.name ILIKE :name", { name: `%${filter.search}%` });
+        if (filter.search) {
+            query = query.where("product.name ILIKE :name", { name: `%${filter.search}%` })
+                .orWhere("product.stock = :stock", { stock: filter.search });
+        }
+
         if (filter.category) query = query.andWhere("category.name like :category", filter);
         return Promise.all([
             query.limit(filter.limit || 10).offset(filter.skip || 0).getMany(),
